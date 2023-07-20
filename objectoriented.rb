9c6c480afa3d61b42d66513728b8219b06e3a28e
james <thuranira611@gmail.com>
2023-07-20 10:57:27 +0300

    create book2 as object 2 an instance of Book class

--- objectoriented.rb
+++ objectoriented.rb
@@ -3,4 +3,15 @@ attr_accessor :title,:author,:pages
 end
 book1 = Book.new()
 book1.title = "live your life" 
+book1.pages = 500
 puts book1.title
+puts book1.pages
+
+book2 = Book.new()
+book2.title = "Never give up in life"
+book2.author = "Thuranira611"
+book2.pages = 700
+
+puts book2.title
+puts book2.author
+puts book2.pages
